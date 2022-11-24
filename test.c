a46a7880048e9cda929cc8778f14e0bb00b14df3
張 貴峰 <zhang.guifeng@nidec.com>
2022-11-24 13:49:07 +0800

    はじめのコミット

---
+++ test.c
@@ -0,0 +1,16 @@
+typedef enum
+{
+    mcReady     = 0, //准备就绪    
+    mcInit      = 1, //初始化     
+    mcCharge    = 2, //充电   
+    mcTailWind  = 3, //逆风顺风判断
+    mcPosiCheck = 4, //位置检查
+    mcAlign     = 5, //对准 ？？
+    mcStart     = 6, //启动    
+    mcRun       = 7, //运行      
+    mcStop      = 8, //停止     
+    mcFault     = 9, //故障处理    
+    mcPllTect   = 10,//刹车  
+    mcBrake     = 11 //刹车    
+}MotStaType;
+
